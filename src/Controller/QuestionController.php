cfcd742317f524c8248dadcbc3fe9a26ea3ae1b3
fsahin03 <302353664@student.rocmondriaan.nl>
2022-03-15 13:34:01 +0100

    twig

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionController
+class QuestionController extends AbstractController
 {
     /**
     * @Route("/")
@@ -20,6 +21,17 @@ class QuestionController
      */
     public function show($slug)
     {
+        $answers = [
+          'Make sure your cat is sitting purrrfectly still XD',
+          'Honestly, I like furry shoes better than MY cat',
+            'Maybe.. try saying the spell backwards?'
+        ];
+
+        return $this->render('question/show.html.twig', [
+            'question' =>  ucwords(str_replace('-', ' ', $slug)),
+            'answers' => $answers
+            ]);
+
         return new Response(sprintf(
             'Future page to show the question "%s"!',
             ucwords(str_replace('-', ' ', $slug))));
