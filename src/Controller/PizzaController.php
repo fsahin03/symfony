b19aecaae2669ff4d8dc7eb41da63801a6453888
fsahin03 <302353664@student.rocmondriaan.nl>
2022-05-18 14:12:39 +0200

    twig

--- src/Controller/PizzaController.php
+++ src/Controller/PizzaController.php
@@ -3,9 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Entity\Order;
+use App\Repository\OrderRepository;
 use App\Repository\PizzasRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Pizza;
@@ -42,16 +47,33 @@ class PizzaController extends AbstractController
     /**
      * @Route ("/order/{id}" , name="order")
      */
-    //    function pizza(EntityManagerInterface $em, $id){
-    //        $pizzas = $em->getRepository(Pizza::class)->findBy(['category_id'=>$id]);
-    //        dd($pizzas);
-    //        //return $this->render('pizza/pizza.html.twig',['pizza'=>$pizza]);
-    //    }
-
-    public function order(EntityManagerInterface $em, $id)
+    public function order(Request $request, Pizza $pizza, OrderRepository $orderRepository)
     {
-        $category=$em->getRepository(Category::class)->find($id);
-        $pizzas=$category->getPizzas();
-        return $this->render('pizza/order.html.twig',['pizzas'=>$pizzas]);
+
+        $order = new Order();
+        $order->setPizza($pizza);
+        $order->setStatus("in progress");
+
+        $form = $this->createFormBuilder($order)
+            ->add("fname", TextType::class, ['label' => 'Voornaam'])
+            ->add("sname", TextType::class, ['label' =>'Achternaam'])
+            ->add('save', SubmitType::class, ['label' => 'Verzenden'])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()&&$form->isValid()){
+            $order = $form->getData();
+
+            $orderRepository->add($order);
+
+        }
+
+
+
+        return $this->renderForm('pizza/order.html.twig', [
+            'form'=> $form
+        ]);
     }
+
 }
